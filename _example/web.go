07c2dc2533c4ca057f2bfe5a93abe9f14fad2cd1
EdcMax <104745596+EdcMax@users.noreply.github.com>
2020-09-15 06:00:49 +0000

    Update composer.lock

--- _example/web.go
+++ _example/web.go
@@ -14,4 +14,4 @@ var addr = flag.String("a", ":8080", "address")
 
 func main() {
 	flag.Parse()
-	http.HandleFunc("/", func(w http.Response
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Requ
