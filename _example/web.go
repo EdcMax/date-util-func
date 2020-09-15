9576a2b7ad4d46c64cb803978b207e41bd6877ee
EdcMax <104745596+EdcMax@users.noreply.github.com>
2020-09-15 20:37:26 +0000

    idiot

--- _example/web.go
+++ _example/web.go
@@ -15,4 +15,4 @@ var addr = flag.String("a", ":8080", "address")
 func main() {
 	flag.Parse()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w
+		fmt.Fprintf(w, "hello "+os.Get
