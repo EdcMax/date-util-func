f8e51c3f07ff8c5a8a7f073ad16faa0d99ac0cc4
EdcMax <104745596+EdcMax@users.noreply.github.com>
2020-09-24 12:26:42 +0000

    Update Tense.md

--- _example/web.go
+++ _example/web.go
@@ -15,4 +15,4 @@ var addr = flag.String("a", ":8080", "address")
 func main() {
 	flag.Parse()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hello "+os.Get
+		fmt.Fprintf(w, "hello "+os.Getenv("AUTHOR"
