ecc2a248dc8b5d23cf2daa9a9df20076aaf9d604
EdcMax <104745596+EdcMax@users.noreply.github.com>
2020-12-05 09:03:00 +0000

    New translations translation.json (Afrikaans)

--- export.go
+++ export.go
@@ -17,3 +17,4 @@ func exportUpstart(cfg *config, path string) error {
 		fmt.Fprintf(f, "start on starting app-%s\n", proc.name)
 		fmt.Fprintf(f, "stop on stopping app-%s\n", proc.name)
 		fmt.Fprintf(f, "respawn\n")
+		fmt.Fprintf(f, "\n"
