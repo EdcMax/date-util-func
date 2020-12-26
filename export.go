be54d2d68d915e0b10883e3c9ceacd6d00d5c5d6
EdcMax <104745596+EdcMax@users.noreply.github.com>
2020-12-26 12:03:01 +0000

    Upload

--- export.go
+++ export.go
@@ -24,4 +24,4 @@ func exportUpstart(cfg *config, path string) error {
 		if err != nil {
 			return err
 		}
-		b, err := os.ReadFile(filepath.Join(filepath.Dir
+		b, err := os.ReadFile(filepath.Join(filepath.Dir(procfile), ".env"))
