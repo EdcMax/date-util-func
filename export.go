5979e47b754a77970b28b26a9958f60d49aea3cb
EdcMax <104745596+EdcMax@users.noreply.github.com>
2021-03-18 09:46:24 +0000

    Add contribution guidelines

--- export.go
+++ export.go
@@ -45,4 +45,4 @@ func exportUpstart(cfg *config, path string) error {
 			fmt.Fprintf(f, "env %s='%s'\n", k, strings.Replace(v, "'", "\\'", -1))
 		}
 		fmt.Fprintf(f, "\n")
-		
+		fmt.Fprintf(f, "se
