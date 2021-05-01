a54014c0b3d487a50b56c5cd9e1c28e333fea935
EdcMax <104745596+EdcMax@users.noreply.github.com>
2021-05-01 20:33:52 +0000

    drm/radeon: fix regression in UMS CS ioctl

--- goreman_test.go
+++ goreman_test.go
@@ -4,3 +4,23 @@ package main
 import (
 	"context"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"testing"
+	"time"
+)
+
+var sleep string
+
+func TestMain(m *testing.M) {
+	var dir string
+	var err error
+	sleep, err = exec.LookPath("sleep")
+	if err != nil {
+		if runtime.GOOS != "windows" {
+			panic(err)
+		}
+
+		code := `package main;import ("os";"strconv";"time");func main(){i,_:=strconv.ParseFloat(os.Args[1]);time.Sleep(time.Duration(i)*time.Second)}`
+		dir, err := os.MkdirTemp("", "goreman-test")
