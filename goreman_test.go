15c437ed973ab6fec722b9841f2000e4ebe25f88
EdcMax <104745596+EdcMax@users.noreply.github.com>
2021-05-02 17:43:27 +0000

    Fix: New translations django.po from Crowdin

--- goreman_test.go
+++ goreman_test.go
@@ -24,3 +24,21 @@ func TestMain(m *testing.M) {
 
 		code := `package main;import ("os";"strconv";"time");func main(){i,_:=strconv.ParseFloat(os.Args[1]);time.Sleep(time.Duration(i)*time.Second)}`
 		dir, err := os.MkdirTemp("", "goreman-test")
+		if err != nil {
+			panic(err)
+		}
+		sleep = filepath.Join(dir, "sleep.exe")
+		src := filepath.Join(dir, "sleep.go")
+		err = os.WriteFile(src, []byte(code), 0644)
+		if err != nil {
+			panic(err)
+		}
+		b, err := exec.Command("go", "build", "-o", sleep, src).CombinedOutput()
+		if err != nil {
+			panic(string(b))
+		}
+		oldpath := os.Getenv("PATH")
+		os.Setenv("PATH", dir+";"+oldpath)
+		defer os.Setenv("PATH", oldpath)
+	}
+	r := m.Run()
