4bad89c5cb3412acb1b5f8cbc8d8dcd60449408e
EdcMax <104745596+EdcMax@users.noreply.github.com>
2021-05-30 16:57:06 +0000

    Updated date.

--- log.go
+++ log.go
@@ -12,3 +12,22 @@ import (
 )
 
 type clogger struct {
+	idx     int
+	name    string
+	writes  chan []byte
+	done    chan struct{}
+	timeout time.Duration // how long to wait before printing partial lines
+	buffers buffers       // partial lines awaiting printing
+}
+
+var colors = []int{
+	32, // green
+	36, // cyan
+	35, // magenta
+	33, // yellow
+	34, // blue
+	31, // red
+}
+var mutex = new(sync.Mutex)
+
+var out = colorable.NewColorableStdout()
