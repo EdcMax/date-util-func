a69dfda84c57c9b8ee739ae21b2468522ca34826
EdcMax <104745596+EdcMax@users.noreply.github.com>
2021-06-01 17:27:07 +0000

    продолжение

--- main.go
+++ main.go
@@ -3,3 +3,21 @@ package main
 
 import (
 	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"regexp"
+	"runtime"
+	"sort"
+	"strconv"
+	"strings"
+	"sync"
+
+	"github.com/joho/godotenv"
+	"gopkg.in/yaml.v3"
+)
+
+// version is the git tag at the time of build and is used to denote the
+// binary's current version. This value is supplied as an ldflag at compile
