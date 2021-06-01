305131089ff07031c9abda1af4f0680c95775316
EdcMax <104745596+EdcMax@users.noreply.github.com>
2021-06-01 21:43:50 +0000

    Improving readme.

--- main.go
+++ main.go
@@ -21,3 +21,24 @@ import (
 
 // version is the git tag at the time of build and is used to denote the
 // binary's current version. This value is supplied as an ldflag at compile
+// time by goreleaser (see .goreleaser.yml).
+const (
+	name     = "goreman"
+	version  = "0.3.13"
+	revision = "HEAD"
+)
+
+func usage() {
+	fmt.Fprint(os.Stderr, `Tasks:
+  goreman check                      # Show entries in Procfile
+  goreman help [TASK]                # Show this help
+  goreman export [FORMAT] [LOCATION] # Export the apps to another process
+                                       (upstart)
+  goreman run COMMAND [PROCESS...]   # Run a command
+                                       start
+                                       stop
+                                       stop-all
+                                       restart
+                                       restart-all
+                                       list
+                                       status
