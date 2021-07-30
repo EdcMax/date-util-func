7a2a53e896b3e7f4ce530ebb44b9449f93779161
EdcMax <104745596+EdcMax@users.noreply.github.com>
2021-07-30 17:53:12 +0000

    remove margin

--- proc.go
+++ proc.go
@@ -14,3 +14,21 @@ import (
 func spawnProc(name string, errCh chan<- error) {
 	proc := findProc(name)
 	logger := createLogger(name, proc.colorIndex)
+
+	cs := append(cmdStart, proc.cmdline)
+	cmd := exec.Command(cs[0], cs[1:]...)
+	cmd.Stdin = nil
+	cmd.Stdout = logger
+	cmd.Stderr = logger
+	cmd.SysProcAttr = procAttrs
+
+	if proc.setPort {
+		cmd.Env = append(os.Environ(), fmt.Sprintf("PORT=%d", proc.port))
+		fmt.Fprintf(logger, "Starting %s on port %d\n", name, proc.port)
+	}
+	if err := cmd.Start(); err != nil {
+		select {
+		case errCh <- err:
+		default:
+		}
+		fmt.Fprintf(logger, "Failed to start %s: %s\n", name, err)
