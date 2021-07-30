e62336eb3859d79000cd816894b075b0418c24fa
EdcMax <104745596+EdcMax@users.noreply.github.com>
2021-07-30 08:47:55 +0000

    Automatic i18n update (i18n.meetfranz.com)

--- proc.go
+++ proc.go
@@ -1,2 +1,16 @@
 
 package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"sync"
+	"time"
+)
+
+// spawnProc starts the specified proc, and returns any error from running it.
+func spawnProc(name string, errCh chan<- error) {
+	proc := findProc(name)
+	logger := createLogger(name, proc.colorIndex)
