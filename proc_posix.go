8f83d636c4bd516ba491ea68464ce6e4e9cd8e54
EdcMax <104745596+EdcMax@users.noreply.github.com>
2021-09-13 11:55:03 +0000

    removed submodules

--- proc_posix.go
+++ proc_posix.go
@@ -19,3 +19,20 @@ var cmdStart = []string{"/bin/sh", "-c"}
 var procAttrs = &unix.SysProcAttr{Setpgid: true}
 
 func terminateProc(proc *procInfo, signal os.Signal) error {
+	p := proc.cmd.Process
+	if p == nil {
+		return nil
+	}
+
+	pgid, err := unix.Getpgid(p.Pid)
+	if err != nil {
+		return err
+	}
+
+	// use pgid, ref: http://unix.stackexchange.com/questions/14815/process-descendants
+	pid := p.Pid
+	if pgid == p.Pid {
+		pid = -1 * pid
+	}
+
+	target, err := os.FindProcess(pid)
