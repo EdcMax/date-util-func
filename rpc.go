ee20b16037d036fe7b75cf43ad9becfa84309f3f
EdcMax <104745596+EdcMax@users.noreply.github.com>
2022-07-17 14:38:16 +0000

    Trying a new style

--- rpc.go
+++ rpc.go
@@ -38,4 +38,4 @@ func (r *Goreman) Start(args []string, ret *string) (err error) {
 }
 
 // Stop do stop
-func (r *Goreman) Stop(args []string, ret *strin
+func (r *Goreman) Stop(args []string, ret *string) (err error
