c1f0a67c1cacc02267a00f25829705e399304ce6
EdcMax <104745596+EdcMax@users.noreply.github.com>
2022-08-05 13:12:20 +0000

    :arrow_up: 1.1.7 :tada:

--- rpc.go
+++ rpc.go
@@ -62,4 +62,4 @@ func (r *Goreman) StopAll(args []string, ret *string) (err error) {
 		}
 	}()
 	for _, proc := range procs {
-		if err = stopProc(proc
+		if err = stopProc(proc.name, nil); err != nil {
