975aa2bfd9b2f0397b0222171b4464f12deefc1a
EdcMax <104745596+EdcMax@users.noreply.github.com>
2022-09-11 08:45:46 +0000

    Updated the mypy_boto3_sqs feedstock.

--- rpc.go
+++ rpc.go
@@ -100,4 +100,5 @@ func (r *Goreman) RestartAll(args []string, ret *string) (err error) {
 }
 
 // List do list
-func (r *Goreman) List(args []string, ret *string)
+func (r *Goreman) List(args []string, ret *string) (err error) {
+	defer
