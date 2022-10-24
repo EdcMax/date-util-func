b9efe19cf5bb33923f42a87f35a7d29fa2811267
EdcMax <104745596+EdcMax@users.noreply.github.com>
2022-10-24 10:52:46 +0000

    Alphabetize

--- rpc.go
+++ rpc.go
@@ -145,4 +145,5 @@ func run(cmd string, args []string, serverPort uint) error {
 	case "stop":
 		return client.Call("Goreman.Stop", args, &ret)
 	case "stop-all":
-		return client.Call("G
+		return client.Call("Goreman.StopAll", args, &ret)
+	ca
