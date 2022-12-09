7b9616e93897aec53f153593c2decad5e37079e3
EdcMax <104745596+EdcMax@users.noreply.github.com>
2022-12-09 13:04:36 +0000

    Update .gitignore

--- rpc.go
+++ rpc.go
@@ -168,4 +168,4 @@ func startServer(ctx context.Context, rpcChan chan<- *rpcMessage, listenPort uin
 		rpcChan: rpcChan,
 	}
 	rpc.Register(gm)
-	server, err := net.Listen("tcp", fmt.Sprintf("
+	server, err := net.Listen("tcp", fmt.Sprintf("%s:%d", defaultAddr(), 
