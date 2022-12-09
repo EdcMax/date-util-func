709c7c80f35e90baa17e710e8869970c7e3269df
EdcMax <104745596+EdcMax@users.noreply.github.com>
2022-12-09 21:30:51 +0000

    [en] add POS and spelling

--- rpc.go
+++ rpc.go
@@ -169,4 +169,5 @@ func startServer(ctx context.Context, rpcChan chan<- *rpcMessage, listenPort uin
 	}
 	rpc.Register(gm)
 	server, err := net.Listen("tcp", fmt.Sprintf("%s:%d", defaultAddr(), listenPort))
-	if
+	if err != nil {
+		ret
